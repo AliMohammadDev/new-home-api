f195351b6af0c629b8c7a0bca14dd37a41461f24
AliMohammadDev <aloshmohammad2001@gmail.com>
2026-01-01 13:50:38 +0300

    fixed in userFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,7 +30,7 @@ class UserFactory extends Factory
       'email' => fake()->unique()->safeEmail(),
       'email_verified_at' => now(),
       'password' => static::$password ??= Hash::make('password'),
-      'role' => $this->faker->randomElement(['admin', 'customer']),
+      'role' => 'customer',
       'remember_token' => Str::random(10),
     ];
   }
