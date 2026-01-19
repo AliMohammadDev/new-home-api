c0c4f6564915467352b84bfcf8113231079c6dd8
AliMohammadDev <aloshmohammad2001@gmail.com>
2026-01-19 17:28:21 +0300

    fixed in product variants

--- app/Http/Resources/ProductVariantResource.php
+++ app/Http/Resources/ProductVariantResource.php
@@ -29,7 +29,7 @@ class ProductVariantResource extends JsonResource
 
       'product' => $this->whenLoaded('product', function () {
         // all images
-
+  
         return [
           'id' => $this->product->id,
           'name' => $this->product->translated_name,
@@ -78,7 +78,7 @@ class ProductVariantResource extends JsonResource
       'final_price' => $this->final_price,
       'current_color' => $this->color ? $this->color->color : null,
       'current_size' => $this->size ? $this->size->size : null,
-      'current_material' => $this->translated_material ? $this->material->translated_material : null,
+      'current_material' => $this->material ? $this->material->translated_material : null,
       'stock_quantity' => $this->stock_quantity,
       'sku' => $this->sku,
       'reviews_avg' => $this->reviews_avg_rating,
