f73e16db72655bc5d71286a3db86bdb99f135e03
AliMohammadDev <aloshmohammad2001@gmail.com>
2026-02-16 17:12:29 +0300

    edit in routes

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Api\SupplierImportController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-  return view('welcome');
+  return redirect('/admin');
 });
 
 
