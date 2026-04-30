38df0d1dfa7619258abc60e64d7cafc6c453b86b
AliMohammadDev <aloshmohammad2001@gmail.com>
2026-04-30 16:18:16 +0300

    make api to  fresh database

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,33 @@ use App\Http\Controllers\Api\FinancialReportController;
 use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Api\ShippingWarehouseController;
 use App\Http\Controllers\Api\SupplierImportController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
   return redirect('/admin');
+});
+
+Route::get('/reset-database', function () {
+  if (app()->environment('local')) {
+
+    Artisan::call('migrate:fresh', [
+      '--seed' => true,
+      '--force' => true,
+    ]);
+
+    return response()->json([
+      'status' => 'success',
+      'message' => 'Database has been cleared, rebuilt, and seeded successfully!',
+      'action' => 'You can now go to the dashboard',
+      'url' => url('/admin')
+    ]);
+  }
+
+  return response()->json([
+    'status' => 'error',
+    'message' => 'Sorry, this command is only available in the Local development environment.'
+  ], 403);
 });
 
 
@@ -23,8 +46,8 @@ Route::get('/shipping/print', [ShippingWarehouseController::class, 'print'])->na
 Route::get('/print-supplier-imports', [SupplierImportController::class, 'print'])
   ->name('supplier.print');
 
-  Route::get('/print-supplier-product-imports', [SupplierImportController::class, 'showProductImport'])
-    ->name('product.import.print');
+Route::get('/print-supplier-product-imports', [SupplierImportController::class, 'showProductImport'])
+  ->name('product.import.print');
 
 
 Route::get('/print-fatora', [FatoraPrintController::class, 'print'])
